57b4fb8bb6537da065ba8649e02a88e7bfbdf1a9
xinkuaiji <40354222+xinkuaiji@users.noreply.github.com>
2023-12-27 16:37:30 +0800

    Update hyperlink.js

--- include/hyperlink.js
+++ include/hyperlink.js
@@ -1,3 +1,4 @@
 ﻿document.write(" <ul>")
 document.write("<li><a href='https://xinkuaiji.github.io/it' target='_blank'><img border='0' src='./images/Connect_logo_3.png' /></a> </li>")
+document.write("<li><a href='https://xinkuaiji.github.io/it' target='_blank'><img border='0' src='./images/Connect_logo_3.png' /></a> </li>")
 document.write(" </ul>")
