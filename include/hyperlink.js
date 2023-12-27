bfce3ac308fa8a1cae3360559ebd0dc46b1c3dc6
xinkuaiji <40354222+xinkuaiji@users.noreply.github.com>
2023-12-27 16:40:22 +0800

    Update hyperlink.js

--- include/hyperlink.js
+++ include/hyperlink.js
@@ -1,4 +1,4 @@
 ﻿document.write(" <ul>")
 document.write("<li><a href='https://xinkuaiji.github.io/it' target='_blank'><img border='0' src='./images/Connect_logo_3.png' /></a> </li>")
-document.write("<li><a href='https://xinkuaiji.github.io/it' target='_blank'><img border='0' src='./images/Connect_logo_3.png' /></a> </li>")
+document.write("<li><a href="http://10.144.192.9/makecode-microbit-setup-win64.exe"  target="_blank">学校内网下载MakeCode</a> </li>")
 document.write(" </ul>")
