127830b33d315d4ad166e6d38389e5392a826954
xinkuaiji <40354222+xinkuaiji@users.noreply.github.com>
2024-07-01 18:23:55 +0800

    Update FollowMe.js

--- include/FollowMe.js
+++ include/FollowMe.js
@@ -1,6 +1,6 @@
 document.write("<h3>联系作者：</h3>")
 document.write("<p>邮箱：<a href='mailto:183328415@qq.com'>183328415@qq.com</a></p>")
 document.write("<p>电话：18983169358</p>")
-document.write("<p><a href='http://25599e7l23.zicp.vip/wordpress/' target='_blank' >My博客</a></p>")
-document.write("<p><a href='http://25599e7l23.zicp.vip/nextcloud/' target='_blank' >NextCloud</a></p>")
+document.write("<p><a href='https://255v99i723.oicp.vip/wordpress/' target='_blank' >My博客</a></p>")
+document.write("<p><a href='https://255v99i723.oicp.vip/nextcloud/' target='_blank' >NextCloud</a></p>")
 document.write("<div class='fakeimg' style='height:20px;'><a href='http://360fbb1a.nat123.fun/' target='_blank' >备用域名nat123</a></div>")
