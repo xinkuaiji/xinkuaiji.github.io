9328136a5db0cb651abf021ad5b9e9db08100ac9
xinkuaiji <40354222+xinkuaiji@users.noreply.github.com>
2025-03-15 16:53:41 +0800

    Update FollowMe.js

--- include/FollowMe.js
+++ include/FollowMe.js
@@ -1,5 +1,4 @@
 document.write("<h3>联系作者：</h3>")
 document.write("<p>邮箱：<a href='mailto:183328415@qq.com'>183328415@qq.com</a></p>")
 document.write("<p>电话：19302349125</p>")
-document.write("<p><a href='http://tangh.gnway.cc/' target='_blank' >金万维：WebDav</a></p>")
 document.write("<div class='fakeimg' style='height:20px;'><a href='https://github.com/xinkuaiji/xinkuaiji.github.io' target='_blank' >网站源码</a></div>")
