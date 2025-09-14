f1f5622c4aebe159e9f79e1bc36d8295bf4cb056
xinkuaiji <40354222+xinkuaiji@users.noreply.github.com>
2025-09-14 14:57:16 +0800

    Update pcmenu.js

--- include/pcmenu.js
+++ include/pcmenu.js
@@ -1,2 +1,2 @@
-document.write("<li><a href='http://daz.unaux.com/pc/xiaoneibisai.php'>校内比赛</a></li>")
-document.write("<li><a href='http://daz.unaux.com/pc/xiaowaibisai.php'>校外比赛</a></li>")
+document.write("<li><a href='http://103.56.114.141/xiaoneibisai.php'>校内比赛</a></li>")
+document.write("<li><a href='http://103.56.114.141/xiaowaibisai.php'>校外比赛</a></li>")
