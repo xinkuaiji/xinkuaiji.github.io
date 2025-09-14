5162946a8524c74ff256ac8cdd7a240c8725b724
xinkuaiji <40354222+xinkuaiji@users.noreply.github.com>
2025-09-14 14:55:05 +0800

    Update acsmenu.js

--- include/acsmenu.js
+++ include/acsmenu.js
@@ -1,2 +1,2 @@
-document.write("<li><a href='http://daz.unaux.com/acs/middle.php'>中段</a></li>")
-document.write("<li><a href='http://daz.unaux.com/acs/upper.php'>高段</a></li>")
+document.write("<li><a href='http://103.56.114.141/middle.php'>中段</a></li>")
+document.write("<li><a href='http://103.56.114.141/upper.php'>高段</a></li>")
